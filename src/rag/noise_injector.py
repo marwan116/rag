bba655fea52931cc237fe9a546d191ea5256311a
Marwan Sarieddine <sarieddine.marwan@gmail.com>
2023-08-28 20:02:01 -0400

    improved draft

--- src/rag/noise_injector.py
+++
@@ -1,9 +0,0 @@
-# inject out-of-context questions and answers
-import pandas as pd
-
-if __name__ == "__main__":
-    df = pd.read_json("~/Downloads/triviaqa-unfiltered/unfiltered-web-train.json")
-    trivia_questions = [row["Question"] for row in df["Data"][:100]]
-    pd.Series(trivia_questions).to_frame("question").to_parquet(
-        "src/rag/qa_trivia_questions.parquet"
-    )
--- src/rag/utils.py
+++ src/rag/utils.py
@@ -1,8 +1,15 @@
-import yaml
 from pathlib import Path
+from typing import Optional
 
-def load_config():
-    config_path = Path(__file__).parent / "config.yaml"
+import yaml
+
+
+def load_config(config_path: Optional[str] = None):
+    config_path = (
+        Path(config_path)
+        if config_path is not None
+        else Path(__file__).parent / "config.yaml"
+    )
     with open(config_path) as f:
         config = yaml.safe_load(f)
     return config
